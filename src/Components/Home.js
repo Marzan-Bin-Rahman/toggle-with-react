413e93c7c1799974cd450511b79e59e2e91ef2fa
MarzanBinRahman <mailhereformarzan@gmail.com>
2022-10-28 16:13:53 +0600

    full done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
     return (
         <div style={{ margin: '0 20px' }}>
             <FAQs faqsData={faqsData} />
+            <p style={{textAlign: 'center', fontSize: '22px', color: '#1bb566'}}>Made with ❤️ by <a style={{textDecoration: 'underline wavy', color: '#fd1'}} href='https://github.com/Marzan-Bin-Rahman'>Marzan</a></p>
         </div>
     )
 }
